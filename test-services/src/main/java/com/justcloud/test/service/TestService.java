87b05671c878072ca7a999cf51603ce43c4eceac
Eduardo Díaz <iamedu@gmail.com>
2012-04-15 23:36:52 -0500

    Adding blueprint services

--- test-services/src/main/java/com/justcloud/test/service/TestService.java
+++ test-services/src/main/java/com/justcloud/test/service/TestService.java
@@ -1,21 +1,42 @@
 package com.justcloud.test.service;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.justcloud.model.test.dao.TestPersonDao;
 
 public class TestService {
 
+	private ScheduledExecutorService executor;
+
+	private final Logger logger = LoggerFactory.getLogger(TestService.class);
+
 	private TestPersonDao testPersonDao;
-	
-	public void setTestPersonDao(TestPersonDao testPersonDao) {
-		Logger.getLogger("TestService").log(Level.INFO,
-				"HOLA " + testPersonDao.toString());
-	}
-	
+
 	public void test() {
-		
+		logger.info("This is ready " + testPersonDao.getAll().size());
+	}
+
+	public void setTestPersonDao(TestPersonDao testPersonDao) {
+		this.testPersonDao = testPersonDao;
+	}
+
+	public void setup() {
+		executor = Executors.newSingleThreadScheduledExecutor();
+		executor.scheduleAtFixedRate(new Runnable() {
+
+			public void run() {
+				test();
+			}
+		}, 10, 10, TimeUnit.SECONDS);
+	}
+
+	public void teardown() {
+		executor.shutdown();
 	}
 
 }
