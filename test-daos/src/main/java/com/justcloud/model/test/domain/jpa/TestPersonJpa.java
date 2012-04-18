b314063a63060fc68bb008fa45c46ae433c7fc31
Eduardo Díaz <iamedu@gmail.com>
2012-04-18 12:54:39 -0500

    Fixing versions and hibernate searcht pus

--- test-daos/src/main/java/com/justcloud/model/test/domain/jpa/TestPersonJpa.java
+++ test-daos/src/main/java/com/justcloud/model/test/domain/jpa/TestPersonJpa.java
@@ -7,10 +7,30 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Version;
 
+import org.apache.lucene.analysis.es.SpanishAnalyzer;
+import org.apache.solr.analysis.PhoneticFilterFactory;
+import org.apache.solr.analysis.StandardFilterFactory;
+import org.apache.solr.analysis.StandardTokenizerFactory;
+import org.hibernate.search.annotations.Analyzer;
+import org.hibernate.search.annotations.AnalyzerDef;
+import org.hibernate.search.annotations.DocumentId;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Fields;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.Parameter;
+import org.hibernate.search.annotations.TokenFilterDef;
+import org.hibernate.search.annotations.TokenizerDef;
+
 import com.justcloud.model.test.domain.TestPerson;
 
 @Entity
-@Table(name="TEST_PERSON")
+@Table(name = "TEST_PERSON")
+@Indexed
+@AnalyzerDef(name = "phonetic", tokenizer = @TokenizerDef(factory = StandardTokenizerFactory.class), filters = {
+		@TokenFilterDef(factory = StandardFilterFactory.class),
+		@TokenFilterDef(factory = PhoneticFilterFactory.class, params = {
+				@Parameter(name = "encoder", value = "DoubleMetaphone"),
+				@Parameter(name = "inject", value = "false") }) })
 public class TestPersonJpa implements TestPerson {
 
 	private static final long serialVersionUID = -6377447941122747890L;
@@ -22,6 +42,7 @@ public class TestPersonJpa implements TestPerson {
 	private Integer version;
 
 	@Id
+	@DocumentId
 	@GeneratedValue
 	public Long getId() {
 		return id;
@@ -32,6 +53,9 @@ public class TestPersonJpa implements TestPerson {
 	}
 
 	@Column(nullable = false)
+	@Fields({
+			@Field(analyzer = @Analyzer(impl = SpanishAnalyzer.class)),
+			@Field(name = "name_phonetic", analyzer = @Analyzer(definition = "phonetic")) })
 	public String getName() {
 		return name;
 	}
@@ -40,6 +64,7 @@ public class TestPersonJpa implements TestPerson {
 		this.name = name;
 	}
 
+	@Field
 	public String getNickname() {
 		return nickname;
 	}
@@ -48,6 +73,7 @@ public class TestPersonJpa implements TestPerson {
 		this.nickname = nickname;
 	}
 
+	@Field
 	public String getEmail() {
 		return email;
 	}
